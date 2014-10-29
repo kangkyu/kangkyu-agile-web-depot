0aa6a1807aa8c0539eca61887cbbf218a898f2cf
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-10-28 19:59:54 -0700

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'b843302f9a091e010a028a5d1f49d9d9fcccbdbec70455aafe53ab095d7934a88d3b02241edf75fcfdea32fdceaea38c6cdd3b807292fa322543dc377b69b7bf'
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create!(
+  title: "Etsy, Inc.",
+  description: 
+    %{<p>
+      Etsy is an e-commerce website focused on handmade 
+      or vintage items and supplies, as well as unique factory-manufactured 
+      items under Etsy's new guidelines, released in October 2013.
+    </p>},
+  image_url: "unknown.jpg",
+  price: 270.25
+)
