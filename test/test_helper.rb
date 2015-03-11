6fdb680c26b73e04e8f6a7ad094b14ff0fdd7644
kangkyu <kangkyu1111@gmail.com>
2015-03-11 14:26:25 -0700

    update gems, ruby version, and color test messages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
+
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
 
